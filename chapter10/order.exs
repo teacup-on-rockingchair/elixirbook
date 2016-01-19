840d02f24787ed3ec670df60d85868fa9cf9ffd6
teacup-on-rockingchair <strandjata@gmail.com>
2016-01-19 20:04:42 +0100

    Remove tabs

--- chapter10/order.exs
+++ chapter10/order.exs
@@ -1,14 +1,14 @@
 defmodule Orders do
 
-	def calc_total(order, nil) do
-		Dict.put(order, :total_amount,  order[:net_amount])
-	end
+  def calc_total(order, nil) do
+    Dict.put(order, :total_amount,  order[:net_amount])
+  end
 
   def calc_total(order, tax_rate) do
     tax = order[:net_amount] * tax_rate
     total = order[:net_amount]+tax
     Dict.put(order, :total_amount, total)
-	end
+  end
 
   def calc_orders(orders, tax_rates) do
     orders |> Enum.map(&calc_total(&1, tax_rates[&1[:ship_to]]))
